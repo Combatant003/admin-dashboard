819090ff486500ce3c69f4608f660cf505aac04c
Avishkar Ghorpade <avishkar.ghorpade@gmail.com>
2024-07-30 00:28:53 +0530

    Added VaR Matrix

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,11 @@ import dash
 from dash import dcc
 from dash import html
 import plotly.express as px
+import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 from dash.dependencies import Input, Output
+from scipy.stats import norm
 from connector import fetch_data_as_dataframe
 
 # Simulate some data
@@ -12,6 +14,16 @@ order_df = fetch_data_as_dataframe('Orders', ['order_id','bid_value', 'ask_value
 user_df = fetch_data_as_dataframe("USER", ['user_id', 'username'])
 # Create a Dash app
 app = dash.Dash(__name__)
+
+def calculate_parametric_var(returns: pd.Series, confidence_level: float = 0.95) -> float:
+    
+    mean = returns.mean()
+    std_dev = returns.std()
+    
+    z_score = norm.ppf(1 - confidence_level)
+    
+    var = mean + z_score * std_dev
+    return var
 
 # Layout of the app
 app.layout = html.Div([
@@ -28,6 +40,8 @@ app.layout = html.Div([
     dcc.Graph(id='exposure-chart'),
     dcc.Graph(id='pnl-chart'),
     dcc.Graph(id='volume-chart'),
+    dcc.Graph(id='live-update-graph'),
+    html.Div(id='var-display'),
     dcc.Interval(
         id='interval-component',
         interval=5*1000,  # in milliseconds
@@ -39,12 +53,14 @@ app.layout = html.Div([
 @app.callback(
     [Output('exposure-chart', 'figure'),
      Output('pnl-chart', 'figure'),
-     Output('volume-chart', 'figure')],
+     Output('volume-chart', 'figure'),
+     Output('live-update-graph', 'figure'),
+     Output('var-display', 'children')],
     [Input('market-maker-dropdown', 'value'),
      Input('interval-component', 'n_intervals')]
 )
 def update_charts(selected_market_maker, n_intervals):
-    order_df = fetch_data_as_dataframe('Orders', ['order_id','bid_value', 'ask_value', 'quantity', 'created_at', 'asset'])
+    order_df = fetch_data_as_dataframe('Orders', ['order_id','bid_value', 'ask_value', 'quantity', 'created_at', 'asset', 'spread'])
     filtered_df = order_df[order_df['asset'] == selected_market_maker]
     
     # Exposure chart
@@ -55,8 +71,17 @@ def update_charts(selected_market_maker, n_intervals):
     
     # Volume chart
     volume_fig = px.line(filtered_df, x='created_at', y='quantity', title='Trading Volume Over Time')
+
+    returns = pd.Series(filtered_df['spread'] * filtered_df['quantity'])
+    VaR = calculate_parametric_var(returns)
+
+    Var_fig = go.Figure()
+
+    Var_fig.add_trace(go.Scatter(x=filtered_df['created_at'], y=filtered_df['quantity'], mode='lines+markers', name='Orders'))
+    var_display = f"Parametric Value at Risk (VaR): {VaR:.2f}"
+
     
-    return exposure_fig, pnl_fig, volume_fig
+    return exposure_fig, pnl_fig, volume_fig, Var_fig, var_display
 
 # Run the app
 if __name__ == '__main__':
